e5cd4dfd967d26a97918716682a11d199f5c8d07
Michael Fernando <mfernando@malwarebytes.com>
2016-11-15 21:42:41 -0600

    Add Angular Devise to Portal Assets

--- app/assets/javascripts/portal-assets.js
+++ app/assets/javascripts/portal-assets.js
@@ -1,6 +1,7 @@
 //= require bootstrap
 //= require angular
 //= require angular-ui-router
+//= require angular-devise
 //= require holderjs
 //= require metisMenu
 //= require startbootstrap-sb-admin-2
